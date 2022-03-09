460ec30853f420c8a6bcf16f0bea7fb779e4bf1c
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-09 15:31:58 +0100

    created script for ticket price calculation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,7 +7,13 @@ function TicketPrice(){
     price.style.display = "block";
 
     finalprice = km*0.21;
+    if (age < 18){
+        finalprice = finalprice - finalprice*20/100;
+    }
+    else if (age >= 65){
+        finalprice = finalprice - finalprice*40/100;
+    }
+    
     finalprice = Math.round(finalprice*100)/100;
     total.innerHTML = `${finalprice} €`;
-
 }
