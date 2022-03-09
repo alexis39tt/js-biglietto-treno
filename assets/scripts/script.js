b3e6994608a7c56aad27cf0bb5dd6257145da943
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-09 15:42:18 +0100

    prettified graphics

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-function TicketPrice(){
+function TicketPrice() {
     let age = document.getElementById("age").value;
     let km = document.getElementById("km").value;
     let price = document.getElementsByClassName("price")[0];
@@ -6,14 +6,14 @@ function TicketPrice(){
 
     price.style.display = "block";
 
-    finalprice = km*0.21;
-    if (age < 18){
-        finalprice = finalprice - finalprice*20/100;
+    finalprice = km * 0.21;
+    if (age < 18) {
+        finalprice = finalprice - finalprice * 20 / 100;
     }
-    else if (age >= 65){
-        finalprice = finalprice - finalprice*40/100;
+    else if (age >= 65) {
+        finalprice = finalprice - finalprice * 40 / 100;
     }
-    
-    finalprice = Math.round(finalprice*100)/100;
+
+    finalprice = Math.round(finalprice * 100) / 100;
     total.innerHTML = `${finalprice} €`;
 }
